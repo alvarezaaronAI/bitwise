06a6dec6e249d87abbcce77de28417bba5f4db10
WongProgrammer <francisnguyen2344@gmail.com>
2020-02-11 15:17:12 -0800

    added the PPMImage class

---
+++ BitWise/src/PPMImage.java
@@ -0,0 +1,32 @@
+package Bitwise;
+
+import java.io.File;
+
+public class PPMImage {
+    String magicNumber;
+    int width;
+    int height;
+    int maxColorValue;
+    char[][][] raster;
+
+    public PPMImage(File Image) {
+
+    }
+
+    public void writeImage(File fileName) {
+
+    }
+
+    public void readImage() {
+
+    }
+
+    public void hideDate(String message) {
+
+    }
+
+    public String recoverDate() {
+        String result = "";
+        return result;
+    }
+}
