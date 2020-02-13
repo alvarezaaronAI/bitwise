9128d1eadf0c19e353760061935f21a1c3493ca9
WongProgrammer <francisnguyen2344@gmail.com>
2020-02-13 14:24:04 -0800

    Triple For Loop

--- BitWise/src/Bitwise/PPMImage.java
+++ BitWise/src/Bitwise/PPMImage.java
@@ -5,12 +5,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class PPMImage {
-    String magicNumber;//First two chars, convert Characters to String
-    int width;//After NL(13) and Return(10)
-    int height;//After Space(32)
-    int maxColorValue;//After NL(13) and Return(10) then after 13 & 10 and the image starts
+    String magicNumber;// First two chars, convert Characters to String
+    int width;// After NL(13) and Return(10)
+    int height;// After Space(32)
+    int maxColorValue;// After NL(13) and Return(10) then after 13 & 10 and the image starts
     char[][][] raster;
 
     public PPMImage(File image) {
@@ -29,43 +31,52 @@ public class PPMImage {
     // Read file image, should be called be the constructor
     private void readImage(File image) throws IOException {
         try {
-            
-            BufferedReader br = new BufferedReader(new FileReader(image));//Reads the file
 
-            this.magicNumber = br.readLine();//Magic Number
+            BufferedReader br = new BufferedReader(new FileReader(image));// Reads the file
+
+            this.magicNumber = br.readLine();// Magic Number
 
             String size = br.readLine();
-            String width = size.substring(0, size.indexOf(' ')); //width
-            String height = size.substring(size.indexOf(' ') + 1, size.length());//height
+            String width = size.substring(0, size.indexOf(' ')); // width
+            String height = size.substring(size.indexOf(' ') + 1, size.length());// height
             this.width = Integer.parseInt(width);
             this.height = Integer.parseInt(height);
 
-            this.maxColorValue = Integer.parseInt(br.readLine()); //Max Color Value
-            
-            // System.out.println(this.magicNumber + " " + this.width + " " + this.height + " " + this.maxColorValue);
+            this.maxColorValue = Integer.parseInt(br.readLine()); // Max Color Value
 
-            this.raster = new char[this.width][this.height][3]; //Raster
+            // System.out.println(this.magicNumber + " " + this.width + " " + this.height +
+            // " " + this.maxColorValue);
 
-            int c;
-            while((c = br.read()) > 0) {//While there is still text in the file, it will read the file character by character
-                
-                System.out.print(c + " ");
-                Character string = new Character((char)c); //Make the int to char to String
-                System.out.println(string.toString()); //Prints out the String
-            }
-            for(int x = this.width; x < raster.length; x++) {
-                for(int y = this.height; y < raster[x].length; y++) {
-                    // if(c = br.read() ) {
-                    //     raster[x][y][0] = (char)c;
-                    // }
+            this.raster = new char[this.width][this.height][3]; // Raster
+
+            // int c;
+            // while((c = br.read()) > 0) {//While there is still text in the file, it will
+            // read the file character by character
+
+            // System.out.print(c + " ");
+            // Character string = new Character((char)c); //Make the int to char to String
+            // System.out.println(string.toString()); //Prints out the String
+            // }
+            for (int x = this.width; x < raster.length; x++) {
+                for (int y = this.height; y < raster[x].length; y++) {
+                    try {
+                        char temp1 = (char) br.read();
+                        char temp2 = (char) br.read();
+                        char temp3 = (char) br.read();
+
+                        this.raster[x][y][0] = temp1;
+                        this.raster[x][y][1] = temp2;
+                        this.raster[x][y][2] = temp3;
+
+                        System.out.println(Arrays.toString(this.raster[x][y]));
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
-
             br.close();
-
-            
         } catch (FileNotFoundException e) {
-            
+
             e.printStackTrace();
         }
     }
@@ -78,6 +89,7 @@ public class PPMImage {
         String result = "";
         return result;
     }
+
     public static void main(String[] args) {
         String fileName = "C:\\PathTest\\cat.ppm";
         File image = new File(fileName);
