f6a846bb59fd4f625104468f6f50a3873b605650
alvarezaaronai <alvarez.a.aaron@gmail.com>
2020-02-17 14:50:23 -0800

    fixed printBinary methid

--- BitWise/src/Bitwise/ReadImage.java
+++ BitWise/src/Bitwise/ReadImage.java
@@ -5,11 +5,12 @@ import java.io.File;
 public class ReadImage {
 
 	   public static void main(String[] args) {
-		   char value = 'M';
-		   System.out.println(binaryPrint(value));
-		   char result = turnBitOff(value, 4);
-		   System.out.println(binaryPrint(result));
-		  
+		   char value = 'F';
+		   System.out.println( value + " : " + binaryPrint(value));
+		   char result = turnBitOff(value, 2);
+		   System.out.println(result + " : " + binaryPrint(result));
+		   //char[] test= { 'A','B','C', 'D','E','F', 'G','H','I', 'J','K', 'L' , '\0'};
+		   //readData(test);
 		   
 
 	    }
@@ -32,7 +33,8 @@ public class ReadImage {
 	  }
 	  
 	  public static int bitStatus(char thisValue, int nBitInput) {
-		char result = (char) (thisValue & onMask(nBitInput));
+		int result = (char) (thisValue & onMask(nBitInput));
+		
 		boolean isBitOn = result > 0;
 		boolean isBitOff = result == 0;
 		if(isBitOn) {
@@ -47,8 +49,26 @@ public class ReadImage {
 	  }
 	  public static String binaryPrint(char valueInput) {
 		  StringBuilder sb = new StringBuilder();
-		  for (int i = 0; i < 8; i++) {
-			sb.append(bitStatus(valueInput, i));
+		  for (int i = 8; i > 0; i--) {
+			int bit = bitStatus(valueInput, i);
+			sb.append(bit);
+		}
+		  return sb.toString();
+	  }
+	  
+	  //read data
+	  public static String readData(char[] pixelsDataInput) {
+		  StringBuilder sb = new StringBuilder();
+		  int index = 0;
+		  
+		  //Create a tempChar and a counter to move along the bits of tempChar
+		  char tempchar = '0';
+		  binaryPrint(tempchar);
+		  //Read Every Character in the Raster.
+		  while (pixelsDataInput[index] != '\0') {
+			char pixel = pixelsDataInput[index];
+			System.out.println(pixel + " : " + binaryPrint(pixel));
+			index++;
 		}
 		  return sb.toString();
 	  }
